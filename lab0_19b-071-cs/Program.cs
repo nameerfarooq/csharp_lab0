c93333b8bb3cbf284c39000d59a30af47e033e38
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-11 00:15:59 +0500

    Add project files.

---
+++ lab0_19b-071-cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
