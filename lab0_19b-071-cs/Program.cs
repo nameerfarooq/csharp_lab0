7bbc4cd229f998644be092f16f385469b54341ab
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-11 02:19:36 +0500

    done lab 0

--- lab0_19b-071-cs/Program.cs
+++ lab0_19b-071-cs/Program.cs
@@ -2,10 +2,10 @@
 
 namespace lab0
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
             //Console.WriteLine("enter 1st number");
             //var a = Convert.ToInt32(Console.ReadLine());
             //Console.WriteLine("enter 2nd number");
@@ -166,7 +166,7 @@ namespace lab0
             //    }
             //    a++;    
             //}
-            bool flag = true;
+            //bool flag = true;
             //do
             //{
             //    Console.WriteLine("enter username");
@@ -207,18 +207,66 @@ namespace lab0
             //{
             //    Console.WriteLine("not found");
             //}
-            
+
+
+            //int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 6, 7, 9 } };
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine("arr[{0},{1}] = {2}", i, j, arr[i, j]);
+            //    }
+            //}
 
 
 
+            //List<int> numbers = new List<int> { 3, 14, 15, 92, 6 };
+            //foreach (int number in numbers)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //Console.WriteLine();
 
 
+            class Box {
+            public double length; // Length of a box
+            public double breadth; // Breadth of a box
+            public double height; // Height of a box
+        }
+        class Boxtester
+        {
+            static void Main(string[] args)
+            {
+                Box Box1 = new Box(); // Declare Box1 of type Box
+                Box Box2 = new Box(); // Declare Box2 of type Box
+                double volume = 0.0; // Store the volume of a box here
+                                     // box 1 specification
+                Box1.height = 5.0;
+                Box1.length = 6.0;
+                Box1.breadth = 7.0;
+                // box 2 specification
+                Box2.height = 10.0;
+                Box2.length = 12.0;
+                Box2.breadth = 13.0;
 
-
-
-
-
-
+                // volume of box 1
+                volume = Box1.height * Box1.length * Box1.breadth;
+                Console.WriteLine("Volume of Box1 : {0}", volume);
+                // volume of box 2
+                volume = Box2.height * Box2.length * Box2.breadth;
+                Console.WriteLine("Volume of Box2 : {0}", volume);
+                Console.ReadKey();
+            }
         }
     }
-}
+
+
+
+
+
+
+
+
+
+    
+    
